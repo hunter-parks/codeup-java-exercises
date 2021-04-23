bf535abef200ada5da5121001c10c9816f018462
Hunter Parks <parks.hunter1992@gmail.com>
2021-04-23 14:50:51 -0500

    feat:completed syntax java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,67 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+        // Question 1
+        int myFavoriteNumber = 23;
+        System.out.println(myFavoriteNumber);
+
+        // Question 2
+        String myString = "I am a string! How Cool!";
+        System.out.println(myString);
+
+        // Question 3
+        // String newString = 'h'; // Doesn't work
+
+        // Question 4
+        // String newStringTwo = 3.14159; // needs to be doubled
+
+        // Question 5
+//        long myNumber;
+        //System.out.println("myNumber = " + myNumber); // does not wor. you need to initialize variable before using it
+
+
+        // Question 6
+        // myNumber = 3.14; // doesn't work
+
+        // Question 9
+        float myNumber = 3.14F ;
+
+        // Question 10
+
+        // This code below adds one post-increment
+//        int x = 5;
+//
+//        System.out.println(myNumber);
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        // This code below adds one pre-increment
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        // Question 11
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+        // Question 12
+
+//        int x = 4;
+//        System.out.println(++x);
+
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(x * y);
+
+        // Question 13
+
+        int x = 10;
+        int y = 2;
+        System.out.println(x/y);
+        System.out.println(y-x);
     }
+
 }
+
