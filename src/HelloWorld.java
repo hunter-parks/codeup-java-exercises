9f366383f66911079eff0f059f8dc2a0b966da01
Hunter Parks <parks.hunter1992@gmail.com>
2021-04-23 15:13:45 -0500

    feat: fixed up some code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -49,18 +49,27 @@ public class HelloWorld {
         // Question 12
 
 //        int x = 4;
-//        System.out.println(++x);
+//        x += 5;
+//        System.out.println("x =" + x );
 
 //        int x = 3;
 //        int y = 4;
-//        System.out.println(x * y);
+//            y *= x;
+//        System.out.println("y = " * y);
 
         // Question 13
 
         int x = 10;
         int y = 2;
-        System.out.println(x/y);
-        System.out.println(y-x);
+        x /= y;
+        y -= x;
+        System.out.println("y = " +y);
+
+        // Question 14
+
+        int z = Integer.MAX_VALUE;
+        z+=1;
+        System.out.println("z = " + z);
     }
 
 }
