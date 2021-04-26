741f60a2e28657eef1a7ea6bc15f61b2dabd78bb
Hunter Parks <parks.hunter1992@gmail.com>
2021-04-26 16:52:21 -0500

    feat: completed up to the powers table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
@@ -31,7 +33,7 @@ public class ControlFlowExercises {
 //        } while (i <= 1000000);
 
         // For Loops
-            // Counts up by 1 to 15
+        // Counts up by 1 to 15
 //        for ( int i = 0; i <= 15; i++){
 //            System.out.println("i is: " + i);
 //        }
@@ -51,5 +53,32 @@ public class ControlFlowExercises {
             System.out.println("i is: " + i);
         }
 
+        // Fizz Buzz Question
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.printf("FizzBuzz\t");
+            } else if ((i % 3 == 0)) {
+                System.out.printf("Fizz\t");
+            } else if ((i % 5 == 0)) {
+                System.out.printf("Buzz\t");
+            } else {
+                System.out.printf("%d\t", i);
+            }
+        }
+
+        //  Powers Table
+        // Wrote a for loop and just printed the questions before looping
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n How many numbers do you want to see? ");
+        int userInt = scanner.nextInt();
+        System.out.println("Number | Squared | Cubed");
+
+        // Looping through numbers and squaring and cubing
+        for (int i = 1; i <= userInt; i++){
+            System.out.println(i + "        " + (i * i) + "         " + "  " +(i * i * i));
+        }
+
     }
 }
+
+
