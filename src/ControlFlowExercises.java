707ed8038c7c0dda4681b40aa0154081397b8220
Hunter Parks <parks.hunter1992@gmail.com>
2021-04-26 15:22:36 -0500

    feat: completed the first section of loops exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,55 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15){
-            System.out.println("i is " + i);
-            i++;
+
+        // While Loop
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println("i is: " + i);
+//            i++;
+//        }
+
+        // Do-While Loop
+        // Counting by 2's
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+
+        // Counting down by 5's
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+
+        // Start from 2 and SQUARE number
+//        double i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i <= 1000000);
+
+        // For Loops
+            // Counts up by 1 to 15
+//        for ( int i = 0; i <= 15; i++){
+//            System.out.println("i is: " + i);
+//        }
+
+//        // Increments by 2
+//        for (int i = 0; i <= 100; i += 2){
+//            System.out.println("i is: " + i);
+//        }
+
+//        // Count down by 5's
+//        for (int i = 100; i >=-10; i -= 5 ) {
+//            System.out.println("i is: " + i);
+//        }
+
+        // SQUARING number
+        for (double i = 2; i <= 1000000; i *= i) {
+            System.out.println("i is: " + i);
         }
+
     }
-
-
-
-
-
 }
