c2e2268a1ab8439e90a1dc5b2fc934a74b55d503
Hunter Parks <parks.hunter1992@gmail.com>
2021-04-26 10:21:30 -0500

    feat: halfway with the exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        System.out.println("Console Exercises");
+        // Question 1
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %s\n", pi );
+        // Question 2
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter an integer");
+//        int number = sc.nextInt();
+
+        // Question 3
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter 3 words!");
+//        int word1 = sc.nextInt();
+//        int word2 = sc.nextInt();
+//        int word3 = sc.nextInt();
+
+        // Question 4
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any sentence");
+        String sentence = sc.next();
+
+
+    }
+}
