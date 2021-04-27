25ad53cb060dab51e1f10c528671a2df6aa5a7a6
Hunter Parks <parks.hunter1992@gmail.com>
2021-04-27 13:47:03 -0500

    feat: completed division problem on Methods

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,7 +2,8 @@ public class MethodsExercises {
     public static void main(String[] args){
 //        System.out.print(returnNum1PlusNum2(2,3)); // ADD
 //        System.out.println(returnNum1MinusNum2(10,5)); //SUBTRACT
-          System.out.println(Num1TimesNum2(3,3));
+//        System.out.println(Num1TimesNum2(3,3)); // MULTIPLY
+          System.out.println(Num1DividedByNum2(100,50)); // DIVIDE
 
     }
 
@@ -25,6 +26,11 @@ public class MethodsExercises {
     }
 
     // DIVISION
+    public static double Num1DividedByNum2(double num1, double num2){
+        double result = num1 / num2;
+        return result;
+    }
+
 
 }
 
