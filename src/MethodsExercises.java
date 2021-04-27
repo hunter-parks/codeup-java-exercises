9b4be237eb52a119e818781c0d14e0a556af7c92
Hunter Parks <parks.hunter1992@gmail.com>
2021-04-27 12:32:59 -0500

    feat: got up to the division problem

---
+++ src/MethodsExercises.java
@@ -0,0 +1,30 @@
+public class MethodsExercises {
+    public static void main(String[] args){
+//        System.out.print(returnNum1PlusNum2(2,3)); // ADD
+//        System.out.println(returnNum1MinusNum2(10,5)); //SUBTRACT
+          System.out.println(Num1TimesNum2(3,3));
+
+    }
+
+    // ADDITION
+    public static double returnNum1PlusNum2(double num1, double num2){
+        double result = num1 + num2;
+        return result;
+    }
+
+    // SUBTRACTION
+    public static double returnNum1MinusNum2(double num1, double num2){
+        double endResult = num1 - num2;
+        return endResult;
+    }
+
+    // MULTIPLICATION
+    public static double Num1TimesNum2(double num1, double num2){
+        double result = num1 * num2;
+        return result;
+    }
+
+    // DIVISION
+
+}
+
