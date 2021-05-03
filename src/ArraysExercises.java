0d08b71f0b9b2cebaaddf420ecf0800196ec112f
Hunter Parks <parks.hunter1992@gmail.com>
2021-05-03 16:54:59 -0500

    feat: Completed first problem of array basics. Onto static method

---
+++ src/ArraysExercises.java
@@ -0,0 +1,18 @@
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+
+                    // Array Basics
+        String[] people = {"Hunter Parks","April Aranda","Leo Parks"};
+        System.out.println();
+        System.out.println(people[1]);
+
+        for (String names : people) {
+            System.out.println("names = " + names);
+        }
+
+
+
+
+    }
+}
