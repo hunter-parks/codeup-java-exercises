8682a3ecab235ac18429d7d159d892244204dd71
Hunter Parks <parks.hunter1992@gmail.com>
2021-05-03 09:30:13 -0500

    feat: Completed first problem on Strings Exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("We don't need no education\n" +
+                "We don't need no thought control \n");
+        System.out.print("Check \"this\" out!, \"s inside of \"s!\n");
+        System.out.print("In windows, the main drive is usually C:\"\n");
+        System.out.print("I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!");
+
+        String pinkFloyd = scanner.next();
+
+
+    }
+}
