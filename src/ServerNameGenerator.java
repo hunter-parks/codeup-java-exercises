8c0cb94f4e567d11a2db25cefbb68798176217ba
Hunter Parks <parks.hunter1992@gmail.com>
2021-05-03 22:24:44 -0500

    feat: updated the name generator. GOT IT WORKING

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,14 +1,18 @@
-import java.util.Arrays;
+
 import java.util.Random;
 
 public class ServerNameGenerator {
     public static void main(String[] args) {
         String[] adjectives = {"brave","eager","delightful","kind","gentle","happy","polite","faithful","witty","zealous"};
         String[] nouns = {"house","cat","dog","country","city","state","guitar","weights","birds","lizards"};
-        Random randomAdjectives = new Random(adjectives.length);
-        Random randomNouns = new Random(nouns.length);
 
-        System.out.println("randomAdjectives = " + randomAdjectives);
-        System.out.println("randomNouns = " + randomNouns);
+        Random r = new Random();
+        int randomAdjective = r.nextInt(adjectives.length);
+        System.out.println(adjectives[randomAdjective]);
+
+        Random random = new Random();
+        int randomNouns = random.nextInt(nouns.length);
+        System.out.println(nouns[randomNouns]);
+
     }
 }
