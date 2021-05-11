eb49622ba2aaa41e4663fd415f2b54371b62409b
Hunter Parks <parks.hunter1992@gmail.com>
2021-05-10 19:26:09 -0500

    feat: did some refactoring on the input test

---
+++ src/util/InputTest.java
@@ -0,0 +1,11 @@
+package util;
+
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input.getString();
+        Input.yesAndNo();
+        Input.getInt(1, 8);
+        Input.getDouble(1, 8);
+    }
+}
